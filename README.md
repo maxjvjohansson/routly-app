2d2351e4496a8d34e5eaab3771aaba8a25c17325
maxjvjohansson <max.jv.johansson@gmail.com>
2025-03-15 22:54:05 +0100

    Setup vite boilerplate. Also optimized project layout

--- README.md
+++
@@ -1,2 +0,0 @@
-# routly-app
-Routly – Smart route planning for cycling &amp; running with real-time weather and traffic insights. Plan, generate, and save optimized routes based on distance, elevation, wind conditions, and more!
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Smarta rutter anpassade efter väder och trafik</h1>
+      <p>Skapa optimerade rutter för cykling eller löpning med realtidsdata om väder och trafikförhållanden.</p>
+    </div>
+  );
+};
+
+export default Home;
